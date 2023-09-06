a82ea19ad61537d309a9050a1d2dceece0ba4628
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-09-06 17:49:21 +0500

    add POST method

--- index.js
+++ index.js
@@ -52,16 +52,51 @@
 // });
 
 //===========================================
-let http = require("http");
+// let http = require("http");
 
-let server = http.createServer((req, res) => {
-  req.url;
-  res.write("<h1>Hellow World 20</h1>");
-  res.write("<h1>404</h1>");
-  res.write("<h1>Hi</h1>");
-  res.end();
+// let server = http.createServer((req, res) => {
+//   req.url;
+//   res.write("<h1>Hellow World 20</h1>");
+//   res.write("<h1>404</h1>");
+//   res.write("<h1>Hi</h1>");
+//   res.end();
+// });
+
+// server.listen(8081, () => {
+//   console.log("create localhost 3000");
+// });
+
+//=========================================
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    res.end(`
+        <h2>Tayping Name:</h2>
+        <form method="post" action="/">
+            <input name="text" type="text" placeholder="name"/>
+            <button type="submit">Push</button>
+        </form>
+        `);
+  } else if (req.method === "POST") {
+    res.writeHead(200, { "content-type": "text/html ; charset=utf-8" });
+
+    let info = [];
+
+    req.on("data", (data) => {
+      info.push(Buffer.from(data));
+    });
+
+    req.on("end", () => {
+      let message = info.toString().split("=")[1];
+
+      res.end(`User name: ${message}`);
+    });
+  }
 });
 
-server.listen(8081, () => {
-  console.log("create localhost 3000");
+server.listen(3000, () => {
+  console.log("localehost:3000");
 });
